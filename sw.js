e392d4c53000fb99d44dc2ea09e23b61a4f45f30
jmbjr <boylejm@gmail.com>
2026-08-09 23:58:32 -0400

    Refresh app cache for statistics and simulations

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='scratched-v28';
+const CACHE='scratched-v30';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.svg','./icon-512.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
